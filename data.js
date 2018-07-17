1a8021f7d11d4250f4bf415059f43c68c909a14a
Caleb Miracle <miracle.caleb@gmail.com>
2018-07-16 23:22:46 -0400

    Style

--- data.js
+++ data.js
@@ -4,7 +4,11 @@ var data = {
         "url": "https://dwrex.files.wordpress.com/2014/02/img_3179_s.jpg",
     }, 
     {
-        "caption": "life finds a way",
-        "url": "https://media1.tenor.com/images/8ce415bb78656fdb7fa96a6032893660/tenor.gif?itemid=5038564"
+        "caption": "Life finds a way",
+        "url": "http://pixel.nymag.com/imgs/daily/vulture/2015/06/12/12-jurassic-park-poop.w529.h352.2x.jpg"
+    },
+    {
+        "caption": "Cocoa for cocoa puffs",
+        "url": "http://4.bp.blogspot.com/_XU9x8G7khv0/SigLZ3HGwzI/AAAAAAAAFCE/lsGLt7bIvg8/s1600/sheep.jpg"
     }]
 }
