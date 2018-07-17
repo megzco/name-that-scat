772a85e426a806fe691f95ffb51ea96efdd1b757
Caleb Miracle <miracle.caleb@gmail.com>
2018-07-16 23:33:55 -0400

    captions

--- data.js
+++ data.js
@@ -10,5 +10,13 @@ var data = {
     {
         "caption": "Cocoa for cocoa puffs",
         "url": "http://4.bp.blogspot.com/_XU9x8G7khv0/SigLZ3HGwzI/AAAAAAAAFCE/lsGLt7bIvg8/s1600/sheep.jpg"
+    },
+    { 
+        "caption": "Pine cone?",
+        "url": "https://www.sciencenewsforstudents.org/sites/default/files/main/blogposts/860%20Fox%20scat.jpg"
+    },
+    {
+        "caption": "Scat-ter brained",
+        "url": "http://4.bp.blogspot.com/_XU9x8G7khv0/SigKytoH2ZI/AAAAAAAAFAs/Hh55oynHiOA/s400/horse-poo.jpg"
     }]
 }
--- mainScat.js
+++ mainScat.js
@@ -7,4 +7,7 @@ var initImage = function() {
     var image = new Image();
     image.src = data.allTheScat[imageIndex].url;
     document.getElementById('image').append(image);    
+
+    var caption = document.createTextNode(data.allTheScat[imageIndex].caption);
+    document.getElementById('caption').append(caption);
 }
