8da88c7eaacaf7a2a07e03e2e1175c3770484e29
Caleb Miracle <d866849@dartcontainer.com>
2018-07-19 13:13:32 -0400

    Guess responses, final message

--- mainScat.js
+++ mainScat.js
@@ -1,13 +1,40 @@
+var guessCount = 0;
+var _data;
+
 window.onload = function() {
+    _data = data;
     initImage();
 };
 
 var initImage = function() {
-    var imageIndex = Math.floor(Math.random() * data.allTheScat.length);        
+    var imageIndex = Math.floor(Math.random() * _data.allTheScat.length);    
     var image = new Image();
-    image.src = data.allTheScat[imageIndex].url;
-    document.getElementById('image').append(image);    
 
-    var caption = document.createTextNode(data.allTheScat[imageIndex].caption);
-    document.getElementById('caption').append(caption);
+    document.getElementById('image').innerHTML = '<img src="' + _data.allTheScat[imageIndex].url + '"/>';
+    document.getElementById('caption').innerHTML = _data.allTheScat[imageIndex].caption;
+
+    _data.allTheScat.splice(imageIndex, 1);
+    console.log(_data.allTheScat);
+}
+
+var submitGuess = function() {
+    document.getElementById('question').classList.add('hidden');
+    
+    if (++guessCount < 5) {
+        document.getElementById('failure').classList.remove('hidden');
+    } else {
+        document.getElementById('success').classList.remove('hidden');
+    }
+}
+
+var tryAgain = function() {
+    document.getElementById('question').classList.remove('hidden');
+    document.getElementById('failure').classList.add('hidden');
+    
+    initImage();
+}
+
+var getPrize = function() {
+    document.getElementById('success').classList.add('hidden');
+    document.getElementById('prize').classList.remove('hidden');    
 }
